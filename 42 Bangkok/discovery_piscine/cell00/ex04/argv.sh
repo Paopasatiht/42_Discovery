ac64351a06b4c6e993e3540c018d21c8906bb257
Paopow <ppasatith2@gmail.com>
2022-09-08 23:18:43 +0700

    update to update everything follow 42 output

--- 42 Bangkok/discovery_piscine/cell00/ex04/argv.sh
+++ 42 Bangkok/discovery_piscine/cell00/ex04/argv.sh
@@ -1,3 +1,30 @@
-echo $1
-echo $2
-echo $3
+# step to accomplish
+# 1. collect the amount of argument
+# 2. if amount == 0; then  echo "it is just 10 bro"
+# 3. elif amount >= 3; then for i in {0..2}; echo args[i]
+# 4. else; then for i in {0..value}; echo args[i]
+
+argument_array=("$@")
+amount_of_argument=$#
+if [ $amount_of_argument == 0 ]
+then
+  echo 'No arguments supplied'
+elif (( $amount_of_argument >= 3 ))
+then
+    for i in {0..2}
+    do
+      echo ${argument_array[i]}
+    done
+elif (( $amount_of_argument == 2 ))
+then
+    for i in 0 1
+    do
+      echo ${argument_array[i]}
+    done
+elif (( $amount_of_argument == 1 ))
+then
+    for i in 0
+    do
+      echo ${argument_array[i]}
+    done
+fi
